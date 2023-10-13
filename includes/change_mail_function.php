21044df6a99e9bb4afec7bc73411fc672c955527
Daniel Abughdyer <fanbase.ng@gmail.com>
2023-10-13 13:42:43 +0100

    Verify nonce for security

--- includes/change_mail_function.php
+++ includes/change_mail_function.php
@@ -1,1 +1,13 @@
 <?php
+
+
+namespace WpChangeAdminEmail;
+
+class WpChangeAdminEmail{
+
+    // Verify nonce for security
+    public function verify_nonce() {
+        if (!wp_verify_nonce($_POST['wp-change-admin-email-test-email-nonce'], 'wp-change-admin-email')) {
+            wp_die('Invalid nonce detected.');
+        }
+    }
